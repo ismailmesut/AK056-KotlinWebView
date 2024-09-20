58fd567e6d6bc40e1c58800782f13e863dc4f7af
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-20 19:19:02 +0300

    updated: web view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinwebview"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinwebview"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinwebview/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinwebview/MainActivity.kt
@@ -2,13 +2,20 @@ package com.ismailmesutmujde.kotlinwebview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinwebview.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
 
-        setContentView(R.layout.activity_main)
+        bindingMain.webView.settings.javaScriptEnabled = true
 
+        bindingMain.webView.loadUrl("https://gelecegiyazanlar.turkcell.com.tr")
     }
 }
